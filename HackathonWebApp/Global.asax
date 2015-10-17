828c6bb8d9d55c2d95a29110891c67384f50c888
Tibeiru Covaci <tibi@covaci.se>
2015-10-17 12:12:19 +0100

    First version of the app

---
+++ HackathonWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackathonWebApp.MvcApplication" Language="C#" %>
---
+++ HackathonWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HackathonWebApp.Startup))]
+namespace HackathonWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
